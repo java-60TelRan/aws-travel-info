57cc0665b5ba9bd54238a94bad44e40a73d87fc9
Yuragranovsky <yuragranovsky@gmail.com>
2026-01-11 22:07:12 +0200

    authorization & snippet for JWT verification inside authentication

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,17 @@
 import logging
 import os
+def getLevelFromEnv(envName: str) -> int:
+    level_name = os.getenv(envName, "INFO").upper()
+    level = logging.getLevelName(level_name)
+
+    if isinstance(level, str):
+        return logging.INFO
+
+    return level
+    
+logging.basicConfig(
+    level=getLevelFromEnv("LOGGER_LEVEL"),
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s"
+)
 logger = logging.getLogger("app")
-levelName: str = os.getenv("LOGGER_LEVEL", "INFO")
-level: int = logging._nameToLevel.get(levelName, logging.INFO)
-logger.setLevel(level)
+logger.info(f"LOGGER LEVEL is {logger.getEffectiveLevel()}")
--- src/open_ai/chat_request.py
+++ src/open_ai/chat_request.py
@@ -1,11 +1,14 @@
 
 import requests
 import os
+from logger import logger
 
 
 URL:str =  "http://localhost:11434/api/chat"
-MODEL_NAME: str = os.getenv("OLLAMA_MODEL_NAME", "phi3")
+MODEL_NAME: str = os.getenv("OLLAMA_MODEL_NAME", "phi3:mini")
 def chatRequest(messages:dict)-> str:
+    print(f"regular print;Chat Request: MODEL_NAME={MODEL_NAME}")
+    logger.debug(f"Chat Request: MODEL_NAME={MODEL_NAME}")
     payload = {
         "model":MODEL_NAME,
         "messages": messages,
